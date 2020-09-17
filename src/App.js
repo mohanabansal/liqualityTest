a271b201f4c2ea618d6adbfa5837116629605f4f
Mohana Bansal <mohanabansal10@gmail.com>
2020-09-17 08:53:37 -0400

    fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 // import logo from "./logo.svg";
+import MarketInfo from "./components/MarketInfo";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Liquality App!!</h1>
+      <MarketInfo />
     </div>
   );
 }
