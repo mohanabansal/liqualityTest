ac8447459f2e561e6500d625c86c9b8eadd2338f
Mohana Bansal <mohanabansal10@gmail.com>
2020-09-17 09:01:25 -0400

    update store

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import Store from "./redux/Store";
+import store from "./redux/Store";
 
 ReactDOM.render(
-  <Provider store={Store}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
