ce7ef7df40861ae8920271e58fff80558eacf89e
Mohana Bansal <mohanabansal10@gmail.com>
2020-09-16 21:37:14 -0400

    test deployment

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          My awesome react app!!! - By MOHANA 
+          My awesome react app!!!
         </p>
       </header>
     </div>
