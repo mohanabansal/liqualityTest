45aa49cdeb965abefd27955b007d6eb5083137b4
Mohana Bansal <mohanabansal10@gmail.com>
2020-09-18 00:29:02 -0400

    add more fields to table

--- src/components/MarketInfo/MarketInfoUI.js
+++ src/components/MarketInfo/MarketInfoUI.js
@@ -27,6 +27,11 @@ function MarketInfoUI(props) {
             <th>From</th>
             <th>To</th>
             <th>Rate</th>
+            <th>Order Expires In</th>
+            <th>Status</th>
+            <th>Max</th>
+            <th>Min</th>
+            <th>Min Conf</th>
           </tr>
           {info.map((item) => {
             return (
@@ -34,6 +39,11 @@ function MarketInfoUI(props) {
                 <td>{item.from}</td>
                 <td>{item.to}</td>
                 <td>{item.rate}</td>
+                <td>{item.orderExpiresIn}</td>
+                <td>{item.status}</td>
+                <td>{item.max}</td>
+                <td>{item.min}</td>
+                <td>{item.minConf}</td>
               </tr>
             );
           })}
